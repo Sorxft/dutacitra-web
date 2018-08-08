87d6174a34c90d3d632764e83a1e980590c79de2
Ryan Patrick Wong <ryanpatrickwong1707@gmail.com>
2018-08-09 01:54:18 +0700

    addded meta name in head

--- site/web/app/themes/sage/resources/views/partials/head.blade.php
+++ site/web/app/themes/sage/resources/views/partials/head.blade.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="google-site-verification" content="7K-PVBw82nCO-kd2FeCE7AnuKjSbK6_C79Pyk-DOKRc" />
   @php wp_head() @endphp
 </head>
