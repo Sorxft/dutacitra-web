2a79301d765655f89720cc3c09002ba4279aeeb3
Ryan Patrick Wong <ryanpatrickwong1707@gmail.com>
2018-08-20 16:21:31 +0700

    added meta description

--- site/web/app/themes/sage/resources/views/partials/head.blade.php
+++ site/web/app/themes/sage/resources/views/partials/head.blade.php
@@ -1,6 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="description" content="Automatic Door, Manusa Automatic doors - Sliding doors, Swing doors, Hermetic doors, Linear doors, Express gates. Want a door installed? Contact Us Now!"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="google-site-verification" content="7K-PVBw82nCO-kd2FeCE7AnuKjSbK6_C79Pyk-DOKRc" />
   @php wp_head() @endphp
