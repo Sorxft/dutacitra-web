31f8c4d292f4fd7e3cd3237ce1358dbad116b298
Ryan Patrick Wong <ryanpatrickwong1707@gmail.com>
2018-08-30 02:07:20 +0800

    changed the map

--- site/web/app/themes/sage/resources/views/template-swingdoors-en.blade.php
+++ site/web/app/themes/sage/resources/views/template-swingdoors-en.blade.php
@@ -226,7 +226,7 @@
       </div>
       <div class="col-md-6" style="padding-right: 30px">
         <div class="embed-responsive" id="map" style="width:100%; height: 100%">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2404.106177147405!2d106.71127686001809!3d-6.148310937122636!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f9f26ee22301%3A0xd48a5a308918c15a!2sPT.+DutaCitra+NusaJaya!5e0!3m2!1sen!2sid!4v1532750663575" width="640" height="480" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.8728037952415!2d106.70993171476879!3d-6.147780395548881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f9f26ee22301%3A0xd48a5a308918c15a!2sAutomatic+Doors%2C+Manusa+Automatic+Doors+-+PT.+DutaCitra+NusaJaya!5e0!3m2!1sid!2sus!4v1535565603463" width="640" height="480" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
       </div>
     </div>
